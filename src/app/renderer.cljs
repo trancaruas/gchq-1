553698413c1f6ea3de10e5a1eed825b24224e519
Vadim Shamray <senweitai@gmail.com>
2016-01-30 14:40:49 +0300

    Basic rules expansion

--- src/app/renderer.cljs
+++ src/app/renderer.cljs
@@ -3,6 +3,8 @@
 
 (declare initial-matrix)
 
+(def log js/console.log)
+
 (defn init []
   (js/console.log "Starting Application"))
 
@@ -74,19 +76,43 @@
                 [1 3 1 4 5 1]    [1 3 1 3 10 2]     [1 3 1 1 6 6]     [1 1 2 1 1 2]       [7 2 1 2 5]]})
 
 ;; Verify that all rules have maximum potential 25 spaces
-(if (not-empty (filter #(> % 25)
-                 (for [rule (:x rules)]
-                   (reduce + (interpose 1 rule)))))
-  (js/console.log "rules for x invalid, has mode then 25 chars")
-  (js/console.log "rules for x valid"))
+(defn validate-rules [s]
+  (if (not-empty (filter #(> % 25)
+                   (for [rule ((keyword s) rules)]
+                     (reduce + (interpose 1 rule)))))
+  (js/console.log (str "rules for " s " invalid, has mode then 25 chars"))
+  (js/console.log (str "rules for " s " valid"))))
 
-(if (not-empty (filter #(> % 25)
-                 (for [rule (:y rules)]
-                   (reduce + (interpose 1 rule)))))
-  (js/console.log "rules for y invalid, has mode then 25 chars")
-  (js/console.log "rules for y valid"))
-
+(validate-rules "x")
+(validate-rules "y")
 
 ;; rule check alg
 ;; (map first (row 4))
 ;; (4 5 13 14 22)
+
+(defn index-rule-x [] (map-indexed (fn [id it] [(inc id) it]) (:x rules)))
+(defn index-rule-y [] (map-indexed (fn [id it] [(inc id) it]) (:y rules)))
+;; ([1 [7 3 1 1 7]] [2 [1 1 2 2 1 1]] [3 [1 3 1 3 1 1 3 1]] [4 [1 3 1 1 6 1 3 1]] [5 [1 3 1 5 2 1 3 1]] [6 [1 1 2 1 1]] [7 [7 1 1 1 1 1 7]] [8 [3 3]] [9 [2 3 1 1 3 1 1 2]] [10 [1 1 3 2 1 1]] [11 [4 1 4 2 1 2]] [12 [1 1 1 1 1 4 1 3]] [13 [2 1 1 1 2 5]] [14 [3 2 2 6 3 1]] [15 [1 9 1 1 2 1]] [16 [2 1 2 2 3 1]] [17 [3 1 1 1 1 5 1]] [18 [1 2 2 5]] [19 [7 1 2 1 1 1 3]] [20 [1 1 2 1 2 2 1]] [21 [1 3 1 4 5 1]] [22 [1 3 1 3 10 2]] [23 [1 3 1 1 6 6]] [24 [1 1 2 1 1 2]] [25 [7 2 1 2 5]])
+
+(defn find-full-x []
+  (map second (filter #(= (first %) 25)
+                (for [rule (:x rules)]
+                  [(reduce + (interpose 1 rule)) rule]))))
+;; ([1 3 1 3 1 3 1 3 1] [7 1 1 1 1 1 7] [1 3 3 2 1 8 1])
+
+(defn expand-rule [r]
+  (->> r
+       (interpose 0)
+       (map #(if (> % 1) (map (constantly 1) (range %)) %))
+       (flatten)))
+;; (expand-rule [7 1 1 1 1 1 7])
+;; (1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1)
+
+(defn expand-x [x r] (map-indexed (fn [id it] [it x (inc id)]) r))
+;; (expand-x 7 (expand-rule [7 1 1 1 1 1 7]))
+;; ([7 1 1] [7 2 1] [7 3 1] [7 4 1] [7 5 1] [7 6 1] [7 7 1] [7 8 0] [7 9 1] [7 10 0] [7 11 1] [7 12 0] [7 13 1] [7 14 0] [7 15 1] [7 16 0] [7 17 1] [7 18 0] [7 19 1] [7 20 1] [7 21 1] [7 22 1] [7 23 1] [7 24 1] [7 25 1])
+
+(defn apply-x [rule]
+  (doseq [r rule
+          :let [c (first r) coor (rest r)]]
+    (if (= 1 c) (apply set-black coor))))
